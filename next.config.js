c36b46757e9980f8113c698fe84a22ede5cd7d95
springhana <xksxksanfro7@naver.com>
2023-12-23 20:51:51 +0900

    create. [Config] image domain add

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: [
+      'avatars.githubusercontent.com',
+      's3.ap-northeast-2.amazonaws.com',
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
